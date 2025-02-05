f202e3515d5e6c693c8edd57b51509d40093b3a4
Hugo Decourcelle <hugodecourcelle@gmail.com>
2025-02-05 09:37:31 +0000

    adding files to run the app online

--- src/minesweeper.py
+++ src/minesweeper.py
@@ -21,7 +21,6 @@ class Minesweeper:
             if (r, c) not in self.mines:
                 self.mines.add((r, c))
                 self.board[r][c] = "💣"
-
         for r, c in self.mines:
             for i in range(r - 1, r + 2):
                 for j in range(c - 1, c + 2):
@@ -35,34 +34,35 @@ class Minesweeper:
                         else:
                             self.board[i][j] += 1
 
-        # Print Board
-        print(10 * "--")
-        for i in range(self.rows):
-            print(self.board[i])
-        print(10 * "--")
-
     def reveal(self, row: int, col: int) -> str:
         """Reveal a cell on the board.
         Any adjacent cells with no mines are also revealed.
         Returns "Game Over" if a mine is revealed, "Continue" otherwise.
         """
-        self.revealed.add((row, col))
         if (row, col) in self.mines:
             return "Game Over"
-        else:
-            return "Continue"
+        self.revealed.add((row, col))
+        if self.board[row][col] == "":
+            self.board[row][col] = "0"
+            for i in range(row - 1, row + 2):
+                for j in range(col - 1, col + 2):
+                    if (
+                        0 <= i < self.rows
+                        and 0 <= j < self.cols
+                        and (i, j) not in self.revealed
+                    ):
+                        self.reveal(i, j)
+        return "Continue"
 
     def get_board(self) -> list:
         """Return the current state of the board."""
-        revealed_board = [
+        return [
             [
-                self.board[r][c] if (r, c) in self.revealed else "?"
+                self.board[r][c] if (r, c) in self.revealed else " "
                 for c in range(self.cols)
             ]
             for r in range(self.rows)
         ]
-        for r in range(self.rows):
-            print(revealed_board[r])
 
     def is_winner(self) -> bool:
         """Check if the game has been won."""
@@ -71,10 +71,3 @@ class Minesweeper:
     def restart(self) -> None:
         """Restart the game with the same parameters."""
         self.__init__(self.rows, self.cols, self.num_mines)
-
-
-if __name__ == "__main__":
-    game = Minesweeper(4, 4, 2)
-    revealed = game.reveal(2, 3)
-    print(revealed)
-    game.get_board()
